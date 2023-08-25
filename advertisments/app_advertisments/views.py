a3ff367f986de8f00bce9f26d61e10e7a8e30d13
Anfisa <anfisag10@gmail.com>
2023-08-25 23:48:15 +0300

    10_Формы

--- advertisments/app_advertisments/views.py
+++ advertisments/app_advertisments/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.urls import reverse
 
 from .forms import AdvertisementForm
 # Create your views here.
@@ -14,6 +15,15 @@ def top_sellers(request):
     return render(request, 'top-sellers.html')
 
 def advertisement_post(request):
-    form = AdvertisementForm()
+    if request.method == "POST":
+        form = AdvertisementForm(request.POST, request.FILES)
+        if form.is_valid():
+            advertisement = Advertisments(**form.cleaned_data)
+            advertisement.user = request.user
+            advertisement.save()
+            url = reverse('main-page')
+            return redirect(url)
+    else:
+        form = AdvertisementForm()
     context = {'form': form}
     return render(request, 'advertisement-post.html', context)
